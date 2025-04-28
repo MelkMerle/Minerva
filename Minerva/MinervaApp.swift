ac2216d1419e10589695d077e43ae63f27af9bbc
MelkMerle <melchior.merlin@gmail.com>
2025-04-28 17:33:48 +0200

    Initial Commit

---
+++ Minerva/MinervaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinervaApp.swift
+//  Minerva
+//
+//  Created by Melchior Merlin on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MinervaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
